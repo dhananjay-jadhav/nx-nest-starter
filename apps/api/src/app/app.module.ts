eaf96ecc3fad484ece843b436a138a2cfde62843
dhananjay-jadhav <jadhav9260@gmail.com>
2025-12-10 22:14:09 +0530

    Added precommit

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 
@@ -29,7 +27,7 @@ import { LoggerModule } from 'nestjs-pino';
             inject: [ConfigService],
         }),
     ],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
