93c209ed654f11d1402fb8b382cd8396173e3a3d
dhananjay-jadhav <jadhav9260@gmail.com>
2025-12-20 13:39:14 +0530

    Added config

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/health/health.module';
+export * from './lib/errors/errors';
+export * from './lib/env-validation/env-validation';
--- libs/utils/src/lib/health/health.module.ts
+++ libs/utils/src/lib/health/health.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 
+import { createValidator } from '../env-validation/env.validation';
+import healthConfig, { HealthVariables } from './health.config';
 import { HealthController } from './health.controller';
 import { ShutdownHealthIndicator } from './shutdown-health-indicator';
 import { ShutdownService } from './shutdown-service.service';
@@ -10,6 +12,8 @@ import { ShutdownService } from './shutdown-service.service';
     imports: [
         ConfigModule.forRoot({
             cache: true,
+            load: [healthConfig],
+            validate: createValidator(HealthVariables),
         }),
         TerminusModule,
     ],
